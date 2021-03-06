d8835ecd39e10843425bae14f7e960d069c82750
Gabriel Bravo <>
2021-03-06 13:12:10 -0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthInterceptor } from '../app/interceptor/auth.interceptor';
+import { AuthenticationService } from '../app/service/authentication.service';
+import { UserService } from '../app/service/user.service';
+import { AuthenticationGuard} from '../app/guard/authentication.guard';
+import { NotificationModule } from './notification.module';
+import { NotificationService } from './service/notification.service';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { UserComponent } from './user/user.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    NotificationModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [NotificationService, AuthenticationGuard, AuthenticationService, UserService,
+    {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/register/register.component.ts
@@ -0,0 +1,60 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { NotificationType } from '../enum/notification-type.enum';
+import { User } from '../model/user';
+import { AuthenticationService } from '../service/authentication.service';
+import { NotificationService } from '../service/notification.service';
+import { HttpErrorResponse } from '@angular/common/http';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+
+  public showLoading: boolean;
+  private subscription: Subscription[] = [];
+
+  constructor(
+    private router: Router,
+    private authenticationService: AuthenticationService,
+    private notificationService: NotificationService
+  ) {}
+
+  ngOnInit(): void {
+    if (this.authenticationService.isUserLoggedIn()) {
+      this.router.navigateByUrl('/user/management');
+    }
+  }
+
+  public onRegister(user: User): void {
+    this.showLoading = true;
+    this.subscription.push(
+      this.authenticationService.register(user).subscribe(
+        (response: User) => {
+          this.showLoading = false;
+          this.sendNotification(NotificationType.SUCCESS, `a new account was created for ${response.firstName}.
+          Please check your email for password to log in.`);
+        },
+        (errorResponse: HttpErrorResponse) => {
+          this.sendNotification(NotificationType.ERROR, errorResponse.error.message);
+          this.showLoading = false;
+        }
+      )
+    )
+  }
+  private sendNotification(notificationType: NotificationType, message: string): void {
+    if (message) {
+      this.notificationService.notify(notificationType, message);
+    } else {
+      this.notificationService.notify(notificationType, 'An error occured. Please try again.');
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.forEach(sub => sub.unsubscribe());
+  }
+
+}
